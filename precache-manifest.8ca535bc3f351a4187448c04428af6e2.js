c70c6ff03851627871e080a54b6d6f0bf81ce8f4
Ali Sattar <alisattarbarani@outlook.com>
2020-10-22 16:31:55 +0300

    Updates

---
+++ precache-manifest.8ca535bc3f351a4187448c04428af6e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dda7914b4b2d45e1fc58ba4efff9f7e",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "b9f29542dc5fb0baf22a",
+    "url": "/my-website/static/css/main.15c2c5a7.chunk.css"
+  },
+  {
+    "revision": "26f9b0c24bc337a104df",
+    "url": "/my-website/static/js/2.70f1615e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-website/static/js/2.70f1615e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9f29542dc5fb0baf22a",
+    "url": "/my-website/static/js/main.e929391b.chunk.js"
+  },
+  {
+    "revision": "8a31e1a8153659a682c8",
+    "url": "/my-website/static/js/runtime-main.0cbcd75a.js"
+  }
+]);
